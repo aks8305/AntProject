b1ce0800146110f3477fe80a466e479ba48500eb
aks8305 <amit.sharma832005@gmail.com>
2020-01-29 12:01:00 +0530

    Update HelloWorld.java

--- src/oata/HelloWorld.java
+++ src/oata/HelloWorld.java
@@ -2,6 +2,7 @@ package oata;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello World Jenkins in AntProject");
+        
     }
 }
